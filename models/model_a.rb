4064fecf14af2733dfc72a00f5413db8a5beab2b
Christian Valesares <cvalesares@gmail.com>
2021-06-11 16:34:37 -0600

    Add finalized model_a.rb

---
+++ models/model_a.rb
@@ -0,0 +1,17 @@
+#hash model of a dino
+# needs: strong naming convention
+# needs: 2 keys => string
+# needs: 2 keys => integer or float
+# 2 keeys boolean
+# 2 keys array
+
+Dinosaur = {
+  'color' => 'green',
+  'species_name' => 'stegasaurus',
+  'weight_in_lbs' => 2600,
+  'height_in_centimeters' => 8720.34,
+  'has_tail' => true,
+  'is_carnivore' => false,
+  'features' => ['tall', 'pointy', 'slow'],
+  'favorite_meals' => ['leaves', 'fruit', 'veggies']
+}
