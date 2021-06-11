4ac47535cac565a1743c648310868eb3fe610f3c
Christian Valesares <cvalesares@gmail.com>
2021-06-11 16:24:11 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,11 @@
+movie = "The Prestige"
+is_entertaining = true
+movie = "Dodgeball"
+num_4 = 4
+num_6 = 6
+product = num_4 * num_6
+favorite_foods = ['pizza', 'chicken shwarma', 'chips']
+
+favorite_foods.shift
+
+puts movie.length
