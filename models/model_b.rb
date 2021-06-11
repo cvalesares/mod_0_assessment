5a1fee364ca43e3afdc736c0c4a7f2f8838b003b
Christian Valesares <cvalesares@gmail.com>
2021-06-11 16:43:02 -0600

    Add finalized model_b.rb

---
+++ models/model_b.rb
@@ -0,0 +1,16 @@
+# hash needs:
+# 2 keys => strings
+# 2 keys => integer or float
+# 2 keys => booleans
+# 2 keys => array
+
+Developer = {
+  'name' => 'Christian Valesares',
+  'nickname' => 'Chri',
+  'height_in_inches' => 74,
+  'weight_in_lbs' => 222.6,
+  'is_new' => true,
+  'knows_everything' => false,
+  'hobbies' => ['hockey', 'bass guitar', 'video games'],
+  'siblings' => ['brother', 'step-sister']
+}
